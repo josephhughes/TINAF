9369e53315314ead05b1fa3c9ace57d66d493494
Joseph Hughes <joseph.hughes@glasgow.ac.uk>
2020-03-25 11:21:06 +0000

    code for the scripts

--- AmpliconDepthSystem.pl
+++ AmpliconDepthSystem.pl
@@ -1,1 +1,63 @@
-../Illumina/batch7c/Script/AmpliconDepthSystem.pl
+#!/usr/bin perl
+
+##################################################################### 
+# A perlscript written by Joseph Hughes, University of Glasgow
+#####################################################################
+# Use this script to determine the depth of each amplicon from a BAM file 
+# perl batch7c/Script/AmpliconDepthSystem.pl -bam batch7c/CVR100_bwa.primerclipped.bam -bedpe batch7c/primerV2.bedpe -out test
+
+use strict;
+use Getopt::Long; 
+use warnings;
+use POSIX;
+#use Bio::SeqIO;
+#use DateTime qw();
+#my $date =DateTime->now->strftime('%Y%m%d');
+
+my ($help,$bam,$out,$bedpe); 
+&GetOptions(
+      'bam:s'  => \$bam, # the bam file to check coverage for 
+      'out:s' => \$out, # the output text tab delimited for each primer pair with coordinate start end and depth 
+      'bedpe:s'  => \$bedpe, # the coordinates of each amplicon in bedpe format
+      'help:s' => \$help,
+           );
+if ($help||!$bam||!$out||!$bedpe){
+  print "Usage: perl batch7c/Script/AmpliconDepthSystem.pl -bam batch7c/CVR100_bwa.primerclipped.bam -bedpe batch7c/primerV2.bedpe -out test\n";
+  print " -bam <bam> - the bam file to check\n";
+  print " -out <txt> - output in text-tab delimited format\n";
+  print " -bedpe <txt> - the coordinates of primers in bedpe format\n";
+  print " -help        - Get this help\n";
+  exit();
+}
+
+my %coord;
+open(BED,"<$bedpe")||die "Can't open $bedpe\n";
+while(<BED>){
+  chomp($_);
+  my @values=split(/\t/,$_);
+  if ($values[0] eq $values[3]){
+    $coord{$values[0]}{$values[6]}{"start"}=$values[1];
+    $coord{$values[0]}{$values[6]}{"end"}=$values[5];
+  }else{
+    print "Different targets for $values[6]: $values[0] and $values[3]\n";
+  }
+}
+
+open(OUT,">$out")||die "Can't open $out\n";
+foreach my $target (keys %coord){
+  foreach my $primer (sort {$coord{$target}{$a}{"start"} <=> $coord{$target}{$b}{"start"}} keys %{$coord{$target}}){
+    my $cmd="samtools depth $bam -r $target:".$coord{$target}{$primer}{"start"}."-".$coord{$target}{$primer}{"end"};
+    #print "$cmd\n";
+    #my $depth=`$cmd`;
+    open (my $pipe_fh, "$cmd |");
+    my $sites=0;
+    my $cumdepth=0;
+    while(<$pipe_fh>){
+      chomp($_);
+      my @depth=split(/\t/,$_);
+      $sites++;
+      $cumdepth=$cumdepth+$depth[2];
+    }
+    print OUT "$primer\t".$coord{$target}{$primer}{"start"}."\t".$coord{$target}{$primer}{"end"}."\t".floor($cumdepth/$sites)."\n";
+  }
+}
--- loopilluminaamplicon.sh
+++ loopilluminaamplicon.sh
@@ -1,1 +1,32 @@
-../Illumina/batch7c/Script/loopilluminaamplicon.sh
+#!/bin/bash
+
+directory=$1
+ref=$2
+bedpe=$3
+
+for filepath in $directory/*R1_001.fastq.gz
+do 
+  echo $filepath
+  file=$(basename $filepath)
+  #echo $file
+  stub=$(echo $file | cut -f1 -d "_")
+  #echo $stub
+  #echo TrimmomaticPE -phred33 $filepath ${filepath%R1_001.fastq.gz}R2_001.fastq.gz ${stub}_trimmomatic_R1.fastq ${stub}_unpaired1.fastq ${stub}_trimmomatic_R2.fastq ${stub}_unpaired2.fastq ILLUMINACLIP:TruSeq2-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:5:25 MINLEN:5 -threads 8 
+#  TrimmomaticPE -phred33 $filepath ${filepath%R1_001.fastq.gz}R2_001.fastq.gz ${stub}_trimmomatic_R1.fastq ${stub}_unpaired1.fastq ${stub}_trimmomatic_R2.fastq ${stub}_unpaired2.fastq ILLUMINACLIP:TruSeq2-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:5:25 MINLEN:5 -threads 8 
+
+  #echo bwa mem $ref ${stub}_trimmomatic_R1.fastq ${stub}_trimmomatic_R2.fastq | samtools view -bS | samtools sort -o ${stub}_bwa.bam
+#  bwa mem $ref ${stub}_trimmomatic_R1.fastq ${stub}_trimmomatic_R2.fastq | samtools view -bS | samtools sort -o ${stub}_bwa.bam
+  
+#  samtools index ${stub}_bwa.bam
+  #echo /home1/orto01r/alicTest/bamclipper-master/bamclipper.sh -b ${stub}_bwa.bam -p $bedpe -s /software/samtools-1.9/bin/samtools
+#  /home1/orto01r/alicTest/bamclipper-master/bamclipper.sh -b ${stub}_bwa.bam -p $bedpe -s /software/samtools-1.9/bin/samtools -n 8
+
+  #echo weeSAM --bam ${stub}_bwa.bam --html ${stub}_bwa
+#  weeSAM --bam ${stub}_bwa.primerclipped.bam --html ${stub}_bwa --overwrite
+  
+  #echo samtools view -F 0x04 -b ${stub}_bwa.bam > ${stub}_bwa_mapped.bam
+#  samtools view -F 0x04 -b ${stub}_bwa.primerclipped.bam | samtools sort  -o  ${stub}_bwa_mapped.bam
+  samtools index ${stub}_bwa_mapped.bam
+  wysiwyg_consensus.pl -bam ${stub}_bwa_mapped.bam -ref $ref -stub ${stub}_cov5 -cov 5
+
+done
